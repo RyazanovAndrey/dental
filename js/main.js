779871f6a78bae09db95f0e33ee34829be872575
RyazanovAndrey <ryazanov.andrej@gmail.com>
2023-09-28 16:33:50 +0300

    16.33

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 20,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
